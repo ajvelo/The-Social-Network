091f2a41b14ac5bd94303b0897d6861006ed602e
Andreas Velounias <andreas.v@hotmail.co.uk>
2019-07-08 14:44:20 +0100

    Add test routes

---
+++ routes/api/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+// @route   GET api/auth
+// @desc    Test route
+// @access  Public
+router.get('/', (req,res) => {
+    return res.send('Auth route');
+});
+
+module.exports = router;
