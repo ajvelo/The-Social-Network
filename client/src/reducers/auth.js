d6eb535b5fdb47dfe8779b310f6977a4823cfbd6
ajvelo <andreas.v@hotmail.co.uk>
2019-07-31 14:20:34 +0100

    Add isAuthenticated state

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,11 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
+} from "../actions/types";
 
 const initialState = {
   token: localStorage.getItem("token"),
@@ -11,6 +18,21 @@ export default function(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
+      };
+    case AUTH_ERROR:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false
+      };
     case REGISTER_SUCCESS:
       localStorage.setItem("token", payload.token);
       return {
@@ -20,6 +42,22 @@ export default function(state = initialState, action) {
         loading: false
       };
     case REGISTER_FAIL:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false
+      };
+    case LOGIN_SUCCESS:
+      localStorage.setItem("token", payload.token);
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false
+      };
+    case LOGIN_FAIL:
       localStorage.removeItem("token");
       return {
         ...state,
