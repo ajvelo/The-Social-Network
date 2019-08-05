e26ec297817ce2d8046161c1a8e250c6a56fbf5e
ajvelo <andreas.v@hotmail.co.uk>
2019-08-05 15:09:41 +0100

    Show profile by id

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import EditProfile from "./components/profile-forms/edit-profile";
 import AddExperience from "./components/profile-forms/add-experience";
 import AddEducation from "./components/profile-forms/add-education";
 import Profiles from "./components/profiles/profiles";
+import Profile from "./components/profile/profile";
 
 // Redux
 import { Provider } from "react-redux";
@@ -40,6 +41,7 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
