f8ba46834c53b7546056ef42be51bf361f76cd32
ajvelo <andreas.v@hotmail.co.uk>
2019-08-06 15:13:23 +0100

    Serve static assets in production

---
+++ client/build/precache-manifest.949b86dcb6206d0866e03663413435b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1340ff05df09d26ca6546b494580b008",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43006279d36425a949ef",
+    "url": "/static/css/main.74e417be.chunk.css"
+  },
+  {
+    "revision": "20b94c36faaf99d545be",
+    "url": "/static/js/2.8ec2b068.chunk.js"
+  },
+  {
+    "revision": "43006279d36425a949ef",
+    "url": "/static/js/main.f7747980.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "d9de0c7ebd1a162ecec65a05212bf075",
+    "url": "/static/media/spinner.d9de0c7e.gif"
+  }
+]);
